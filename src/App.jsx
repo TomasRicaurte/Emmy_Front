ef4c3d0303f287e9020996770c8b8aa2a6b7495a
Tomas Ricaurte <tomiychiqui@gmail.com>
2023-11-24 10:56:46 -0500

    Comienzo de la app con los componentes definidos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Box } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
